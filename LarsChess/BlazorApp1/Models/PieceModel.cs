185188a5b2380645db3c1395b8e9331de001537d
Lars Jensen <Lars@gkkg.no>
2023-07-10 00:14:29 +0200

    working pieces dragndrop

---
+++ LarsChess/BlazorApp1/Models/PieceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorApp1.Models
+{
+    public enum PieceClass { King, Queen, Bishop, Knight, Rook, Pawn}
+    public class PieceModel
+    {
+        public PieceClass Class { get; set; }
+        public int Id { get; set; }
+        public int[] Position {  get; set; }
+    }
+}
