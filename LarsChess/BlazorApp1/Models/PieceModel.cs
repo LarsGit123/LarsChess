a6525478dc0ba0afeec6db01e961fc54025801de
Lars Jensen <Lars@gkkg.no>
2023-07-12 22:00:40 +0200

    working drag and drop

--- LarsChess/BlazorApp1/Models/PieceModel.cs
+++ LarsChess/BlazorApp1/Models/PieceModel.cs
@@ -3,10 +3,19 @@
     public enum PieceClass { King, Queen, Bishop, Knight, Rook, Pawn}
     public class PieceModel
     {
-        public PieceClass Class { get; set; }
+        public PieceClass PieceClass { get; set; }
         public string Id { get; set; }
         public int[] Position {  get; set; }
 
         public int[] OrgPosition { get; set; }
+
+        public PieceModel(int[] position, string id, PieceClass pieceClass)
+        {
+            Position = position;
+            OrgPosition = position;
+            Id = id;
+            PieceClass = pieceClass;
+        }
     }
+    
 }
