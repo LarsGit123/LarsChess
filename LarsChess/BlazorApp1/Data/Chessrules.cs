c42f7b01fa7d3cff5c6ea176e8b55e3e4aac325d
Lars Jensen <Lars@gkkg.no>
2023-07-13 21:32:22 +0200

    piece class and colour. Legal moves

---
+++ LarsChess/BlazorApp1/Data/Chessrules.cs
@@ -0,0 +1,67 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1.Data
+{
+    public static class Chessrules
+    {
+        public static Dictionary<(int, int), (PieceClass, Colour)> StartingPositions { get; private set; } = new Dictionary<(int, int), (PieceClass, Colour)>
+        {
+            { (0,0), (PieceClass.Rook, Colour.White) },
+            { (1,0), (PieceClass.Bishop, Colour.White) },
+            { (2,0), (PieceClass.Knight, Colour.White) },
+            { (3,0), (PieceClass.Queen, Colour.White) },
+            { (4,0), (PieceClass.King, Colour.White) },
+            { (5,0), (PieceClass.Knight, Colour.White) },
+            { (6,0), (PieceClass.Bishop, Colour.White) },
+            { (7,0), (PieceClass.Rook, Colour.White) },
+            { (0,1), (PieceClass.Pawn, Colour.White) },
+            { (1,1), (PieceClass.Pawn, Colour.White) },
+            { (2,1), (PieceClass.Pawn, Colour.White) },
+            { (3,1), (PieceClass.Pawn, Colour.White) },
+            { (4,1), (PieceClass.Pawn, Colour.White) },
+            { (5,1), (PieceClass.Pawn, Colour.White) },
+            { (6,1), (PieceClass.Pawn, Colour.White) },
+            { (7,1), (PieceClass.Pawn, Colour.White) },
+            
+            { (0,6), (PieceClass.Pawn, Colour.Black) },            
+            { (1,6), (PieceClass.Pawn, Colour.Black) },
+            { (2,6), (PieceClass.Pawn, Colour.Black) },
+            { (3,6), (PieceClass.Pawn, Colour.Black) },
+            { (4,6), (PieceClass.Pawn, Colour.Black) },
+            { (5,6), (PieceClass.Pawn, Colour.Black) },
+            { (6,6), (PieceClass.Pawn, Colour.Black) },
+            { (7,6), (PieceClass.Pawn, Colour.Black) },
+            { (0,7), (PieceClass.Rook, Colour.Black) },
+            { (1,7), (PieceClass.Bishop, Colour.Black) },
+            { (2,7), (PieceClass.Knight, Colour.Black) },
+            { (3,7), (PieceClass.Queen, Colour.Black) },
+            { (4,7), (PieceClass.King, Colour.Black) },
+            { (5,7), (PieceClass.Knight, Colour.Black) },
+            { (6,7), (PieceClass.Bishop, Colour.Black) },
+            { (7,7), (PieceClass.Rook, Colour.Black) },
+        };
+    
+        public static List<(int,int)> GetLegalMoves(PieceClass piece, Colour colour, (int x,int y) p)
+        {
+            var moves = new List<(int,int)>();
+            switch(piece)
+            {
+                case PieceClass.King:
+                    if (p.x < 7 && p.y>1)   moves.Add((p.x + 1, p.y - 1));
+                    if (p.x < 7)            moves.Add((p.x + 1, p.y));
+                    if (p.x < 7 && p.y <7)  moves.Add((p.x + 1, p.y + 1));
+
+                    if (p.x > 1 && p.y > 1) moves.Add((p.x - 1, p.y - 1));
+                    if (p.x > 1)            moves.Add((p.x - 1, p.y));
+                    if (p.x > 1 && p.y < 7) moves.Add((p.x - 1, p.y + 1));
+
+                    if (p.y < 7)            moves.Add((p.x, p.y + 1));
+                    if (p.y > 1)            moves.Add((p.x, p.y - 1));
+                    break;
+
+            }
+            return moves;
+        }
+        
+    }
+}
--- LarsChess/BlazorApp1/Models/PieceModel.cs
+++ LarsChess/BlazorApp1/Models/PieceModel.cs
@@ -1,20 +1,23 @@
 ﻿namespace BlazorApp1.Models
 {
     public enum PieceClass { King, Queen, Bishop, Knight, Rook, Pawn}
+    public enum Colour { White, Black}
     public class PieceModel
     {
+        public Colour Colour { get; set; }
         public PieceClass PieceClass { get; set; }
         public string Id { get; set; }
         public int[] Position {  get; set; }
 
         public int[] OrgPosition { get; set; }
 
-        public PieceModel(int[] position, string id, PieceClass pieceClass)
+        public PieceModel(int[] position, string id, PieceClass pieceClass, Colour colour)
         {
             Position = position;
             OrgPosition = position;
             Id = id;
             PieceClass = pieceClass;
+            Colour = colour;
         }
     }
     
